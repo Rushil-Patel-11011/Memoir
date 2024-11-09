fe718fa790b743a81de14e6b4d364049194085dd
Rushil Kakadiya <rushilpatel11011@gmail.com>
2024-11-09 11:01:00 +0530

    5th commit : in this commit I learned how to make transaction using @Transactional annotation in SpringBoot.
    Topics I learned...
    -> PlatformTransactionManager interface and it's implementation in MongoTransactionManager.
    -> use of @EnableTransactionManagement annotation and @Bean annotation in springboot.

--- src/main/java/com/rushil/Memoir/service/JournalEntryService.java
+++ src/main/java/com/rushil/Memoir/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import com.rushil.Memoir.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.*;
@@ -19,6 +20,7 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
         try{
             User user = userService.findByUserName(userName);
@@ -28,7 +30,8 @@ public class JournalEntryService {
             userService.saveEntry(user);
         } catch(Exception e)
         {
-            System.out.println("Exception"+e);
+            System.out.println("Exception : "+e);
+            throw new RuntimeException("An error occur while saving the entry.", e);
         }
     }
 
