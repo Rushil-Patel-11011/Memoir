d2e1735bd93eb4642208635f07f47b1c7eab2a63
Rushil Kakadiya <rushilpatel11011@gmail.com>
2024-11-21 11:33:21 +0530

    8th commit : in this commit I learned how to add role based authentication according to the user's role in SpringBoot.
    Topics I learned...
    -> use of Authentication interface and it's methods

---
+++ src/main/java/com/rushil/Memoir/controller/AdminController.java
@@ -0,0 +1,31 @@
+package com.rushil.Memoir.controller;
+
+import com.rushil.Memoir.entity.User;
+import com.rushil.Memoir.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all-users")
+    public ResponseEntity<?> getAllUser() {
+        List<User> all = userService.getAll();
+        if (all != null && !all.isEmpty()) {
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(all, HttpStatus.NOT_FOUND);
+    }
+    @PostMapping("create-admin")
+    public void createAdmin(@RequestBody User user)
+    {
+        userService.saveNewAdmin(user);
+    }
+}
